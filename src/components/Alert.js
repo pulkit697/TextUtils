6e6a6eb51eabb5aa2be0eb8bf426d1eaedc4b416
Pulkit-Windows <pulkitgarg697@gmail.com>
2021-11-17 15:55:07 +0530

    class to className

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export const Alert = (props) => {
   return (
     <div style={{ height: "50px" }}>
       {props.alert && (
-        <div class={`alert alert-${props.alert.type}`} role="alert">
+        <div className={`alert alert-${props.alert.type}`} role="alert">
           <strong>{props.alert.heading}</strong>
           &nbsp;{props.alert.message}
         </div>
